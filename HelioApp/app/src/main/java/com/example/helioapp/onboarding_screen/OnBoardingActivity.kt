6e9210d2a7296e726160bcea72dd71b055eec279
jay-a-shah <jay.a.shah@simformsolutions.com>
2022-05-10 20:58:16 +0530

    OnBoarding Screen Added

--- HelioApp/app/src/main/java/com/example/helioapp/onboarding_screen/OnBoardingActivity.kt
+++ HelioApp/app/src/main/java/com/example/helioapp/onboarding_screen/OnBoardingActivity.kt
@@ -2,16 +2,38 @@ package com.example.helioapp.onboarding_screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.databinding.DataBindingUtil
 import com.example.helioapp.R
 import com.example.helioapp.databinding.ActivityOnBoardingBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class OnBoardingActivity : AppCompatActivity() {
 
+    var onBoardingItemList: ArrayList<OnBoardingModel> = arrayListOf()
     lateinit var binding: ActivityOnBoardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_on_boarding)
-
+        setAdapter()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        TabLayoutMediator(binding.tabLayout, binding.viewPager2) { tab, position ->
+        }.attach()
+    }
+    fun setAdapter(){
+        setData()
+        binding.viewPager2.adapter = OnBoardingAdapter(onBoardingItemList)
+    }
+    fun setData(){
+        onBoardingItemList.apply {
+            add(OnBoardingModel(R.drawable.background_image,getString(R.string.header_one),getString(
+                R.string.subHeader)))
+            add(OnBoardingModel(R.drawable.onboarding_two,getString(R.string.header_two),getString(
+                R.string.subHeader)))
+            add(OnBoardingModel(R.drawable.onboarding_one_hdpi,getString(R.string.header_three),getString(
+                R.string.subHeader)))
+        }
     }
 }
--- HelioApp/app/src/main/java/com/example/helioapp/onboarding_screen/OnBoardingAdapter.kt
+++ HelioApp/app/src/main/java/com/example/helioapp/onboarding_screen/OnBoardingAdapter.kt
@@ -9,19 +9,20 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.helioapp.R
 import com.example.helioapp.databinding.ItemOnBoardingScreenBinding
 
-class OnBoardingAdapter(val list: ArrayList<OnBoardingAdapter>): RecyclerView.Adapter<OnBoardingAdapter.ViewPagerHolder>() {
+class OnBoardingAdapter(val list: ArrayList<OnBoardingModel>): RecyclerView.Adapter<OnBoardingAdapter.ViewPagerHolder>() {
+
     lateinit var binding: ItemOnBoardingScreenBinding
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerHolder {
-        val viewHolder = ItemOnBoardingScreenBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return ViewPagerHolder(viewHolder)
+        binding = ItemOnBoardingScreenBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewPagerHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: OnBoardingAdapter.ViewPagerHolder, position: Int) {
-       // binding.itemImage =
+    override fun onBindViewHolder(holder: ViewPagerHolder, position: Int) {
+        binding.onBoardingViewModel = list[position]
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return list.size
     }
 
     class ViewPagerHolder(binding: ItemOnBoardingScreenBinding) : RecyclerView.ViewHolder(binding.root) {
