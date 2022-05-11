25aa487144ae2d60ff3119af5a5b30a13e90cbe9
jay-a-shah <jay.a.shah@simformsolutions.com>
2022-05-11 20:45:52 +0530

    Sign Up Screen Started

---
+++ HelioApp/app/src/main/java/com/example/helioapp/signup_screen/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.example.helioapp.signup_screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.helioapp.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+
+}
