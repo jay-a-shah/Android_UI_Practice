29628671eccb285a0b32d2bc7d5a0625aa11ed7a
jay-a-shah <jay.a.shah@simformsolutions.com>
2022-05-16 19:12:28 +0530

    Changes Added

---
+++ HelioApp/app/src/main/java/com/example/helioapp/home_screen/HomeScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.helioapp.home_screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.helioapp.R
+
+class HomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
